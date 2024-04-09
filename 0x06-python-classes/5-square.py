70afbfcb8da76e0ab01ba61d4b2ed1a5ee1c911f
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2024-04-09 13:25:06 +0100

    commit

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""_summary_
+
+    Raises:
+        TypeError: _description_
+        ValueError: _description_
+        TypeError: _description_
+        ValueError: _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+
+class Square:
+    """_summary_
+    """
+    def __init__(self, size=0):
+        """_summary_
+
+        Args:
+            size (int, optional): _description_. Defaults to 0.
+
+        Raises:
+            TypeError: _description_
+            ValueError: _description_
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def area(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        if self.__size == 0:
+            print("")
+            return None
+
+        for i in range(1, self.area() + 1, 1):
+            print('#', end="")
+            if i % self.__size == 0 and i > 0:
+                print("")

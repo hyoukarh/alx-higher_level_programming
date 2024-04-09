88a87ea4e31f1c764dc28b0a6f3f63bc55dbc073
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2024-04-09 13:21:01 +0100

    commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Square Class
+A square class
+"""
+
+
+class Square():
+    """__init__
+
+    The __init__ method .....
+
+    Attributes:
+        size(int): .......
+    """
+    def __init__(self, size):
+        self.__size = size

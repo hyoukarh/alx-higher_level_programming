7ac072cae61b632fdb32a6827d87aed088dcafde
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2024-06-21 12:00:28 +0100

    commit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-/* node is always the first argument */
 if (process.argv && process.argv.length > 2) {
     console.log('Arguments found');
   } else if (process.argv && process.argv.length === 3) {
